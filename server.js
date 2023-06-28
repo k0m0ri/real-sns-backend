77facbfdbee51c4c6292a4da04914a8631abc09d
H_Komori <komori.sc14@gmail.com>
2023-06-29 00:21:23 +0900

    ユーザー用のルーティング設定

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-console.log("node.js");
+const express = require("express");
+const app = express();
+const userRoute = require("./routes/users");
+const PORT = 3000;
+
+// ミドルウェア
+app.use("/api/users", userRoute);
+
+app.listen(PORT, () => console.log("サーバーが起動しました"));

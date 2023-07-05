8856ccb1b1c2509115c8da00fd83e55a4588db76
H_Komori <komori.sc14@gmail.com>
2023-07-05 20:23:04 +0900

    ユーザーのCRUD操作を作成

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,48 @@
-const router = require("express").Router();
 const User = require("../models/User");
+const router = require("express").Router();
+
+// CRUD
+// ユーザー情報の更新
+// :id -> 任意のIDを示す
+router.put("/:id", async(req, res) => {
+    if(req.body.userId === req.params.id || req.body.isAdmin) {
+        try {
+            const user = await User.findByIdAndUpdate(req.params.id, {
+                // $->Schemaのパラメーター全指定
+                $set: req.body,
+            });
+            res.status(200).json("ユーザー情報が更新されました");
+        } catch(err) {
+            return res.status(500).json(err);
+        }
+    } else {
+        return res.status(403).json("アカウントIDが異なるのでユーザー情報を更新できません");
+    }
+});
+
+// ユーザー情報の削除
+router.delete("/:id", async(req, res) => {
+    if(req.body.userId === req.params.id || req.body.isAdmin) {
+        try {
+            const user = await User.findByIdAndDelete(req.params.id);
+            res.status(200).json("ユーザー情報が削除されました");
+        } catch(err) {
+            return res.status(500).json(err);
+        }
+    } else {
+        return res.status(403).json("アカウントIDが異なるのでユーザー情報を削除できません");
+    }
+});
+
+// ユーザー情報の取得
+router.get("/:id", async(req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        const { password, updateAt, ...other} = user._doc;
+        res.status(200).json(other);
+    } catch(err) {
+        return res.status(500).json(err);
+    }
+});
 
 module.exports = router;
